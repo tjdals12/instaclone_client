35080dea050f527b03b850887cab41a741311a7e
Seongmin <lll2slll@naver.com>
2020-01-23 13:51:56 +0900

    홈페이지 완료

--- src/components/HomeBackground/HomeBackground.tsx
+++ src/components/HomeBackground/HomeBackground.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classNames from 'classnames/bind';
 import HomeBackground from 'assets/img/home.png';
 import slideImages from 'assets/img/slide';
@@ -7,6 +7,28 @@ import styles from './HomeBackground.module.scss';
 const cx = classNames.bind(styles);
 
 export default function BackgroundBox(): React.ReactElement {
+    const slideRef = useRef<HTMLDivElement>();
+
+    useEffect(() => {
+        const slide = slideRef.current.children;
+
+        let i = 0;
+
+        const slideInterval = setInterval(() => {
+            slide.item(i).classList.remove(cx('show'));
+            slide
+                .item(i + 1 === slide.length ? 0 : i + 1)
+                .classList.add(cx('show'));
+            i += 1;
+
+            if (i === slide.length) i = 0;
+        }, 5000);
+
+        return (): void => {
+            clearInterval(slideInterval);
+        };
+    }, []);
+
     return (
         <div
             className={cx('background')}
@@ -14,11 +36,11 @@ export default function BackgroundBox(): React.ReactElement {
                 backgroundImage: `url(${HomeBackground})`,
             }}
         >
-            <div className={cx('slide')}>
+            <div className={cx('slide')} ref={slideRef}>
                 {slideImages.map((img, index) => (
                     <img
                         key={index}
-                        className={cx(`slide-image`, index !== 1 ? 'hide' : '')}
+                        className={cx(`slide-image`, index === 0 ? 'show' : '')}
                         src={img}
                         alt={`슬라이드 이미지${index}`}
                     />
